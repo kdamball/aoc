6494bba29c89e244ba0409fac2e063cdfc84f067
Kado Damball <kadodamball@hotmail.com>
2023-01-07 18:40:57 +0100

    09.2

--- 09.ts
+++ 09.ts
@@ -1,70 +1,55 @@
 import { cleanInput } from "./utils";
 
-const input = `R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2`;
+const input = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`;
 
 
 const insts = cleanInput(input).map(e => e.split(' ')).map(e => ({move: e[0], dist: Number(e[1])}));
 
-let tail = [0, 0];
-let head = [0, 0];
+let longRope = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
 
-const tailVisits = [tail.join(',')];
+const tailVisits = [longRope.at(-1).join(',')];
 
 insts.forEach(inst => {
+  let head, tail;
   for (let i = 0; i<inst.dist; i++) {
-    switch (inst.move){
-      case 'R':
-        head = [head[0], ++head[1]];
-        if (Math.abs(head[1] - tail[1]) > 1) {
-          if (tail[1] !== head[1]) {
-            tail = [head[0], tail[1]]
-          }
-          ++tail[1];
+    for (let ind = 0; ind < (longRope.length - 1); ind++) {
+      head = longRope[ind];
+      tail = longRope[ind + 1];
+
+      switch (inst.move){
+        case 'R':
+          if (ind === 0){ ++head[1] };
+          break;
+        case 'L':
+          if (ind === 0){ --head[1] };
+          break;
+        case 'U':
+          if (ind === 0){ --head[0]; }
+          break;
+        case 'D':
+          if (ind === 0){ ++head[0] };
+          break;
+      }
+
+      if (Math.abs(head[0] - tail[0]) > 1 || Math.abs(head[1] - tail[1]) > 1) {
+        if (head[0] !== tail[0]) {
+          tail[0] += head[0] > tail[0] ? 1 : -1;
         }
-        // console.log(inst, head, tail);
-        break;
-      case 'L':
-        head = [head[0], --head[1]];
-        if (Math.abs(head[1] - tail[1]) > 1) {
-          if (tail[1] !== head[1]) {
-            tail = [head[0], tail[1]]
-          }
-          --tail[1];
+        if (head[1] !== tail[1]) {
+          tail[1] += head[1] > tail[1] ? 1 : -1;
         }
-        // console.log(inst, head, tail);
-        break;
-      case 'U':
-        head = [--head[0], head[1]];
-        if (Math.abs(head[0] - tail[0]) > 1) {
-          if (tail[1] !== head[1]) {
-            tail = [tail[0], head[1]]
-          }
-          --tail[0];
-        }
-        // console.log(inst, head, tail);
-        break;
-      case 'D':
-        head = [++head[0], head[1]];
-        if (Math.abs(head[0] - tail[0]) > 1) {
-          if (tail[1] !== head[1]) {
-            tail = [tail[0], head[1]]
-          }
-          ++tail[0];
-        }
-        // console.log(inst, head, tail);
-        break;
+      };
     }
-    if (!tailVisits.includes(tail.join(','))) {
-      tailVisits.push(tail.join(','));
+    if (!tailVisits.includes(longRope.at(-1).join(','))) {
+      tailVisits.push(longRope.at(-1).join(','));
     }
   }
 });
-
 console.log(tailVisits.length);
