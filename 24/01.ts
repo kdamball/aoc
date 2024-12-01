25f255d0e1fc56027ff63be7437cb47e67c0e289
Kado <kadodamball@hotmail.com>
2024-12-01 17:33:08 +0100

    01

---
+++ 24/01.ts
@@ -0,0 +1,48 @@
+import { cleanInput, sortNumber } from "./utils";
+
+// part 1
+{
+  const rawInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+  const {left, right} = cleanInput(rawInput).map(row => row.split(/\s+/).map(Number)).reduce((agg, curr) => {
+    const [l, r] = curr
+    agg.left.push(l)
+    agg.right.push(r)
+    return agg
+  }, {left: [], right: []})
+  const [sLeft, sRight] = [left.sort(sortNumber), right.sort(sortNumber)]
+  const diff = sLeft.reduce((prev, curr, index) => {
+    return prev + Math.abs(curr - sRight[index])
+  }, 0)
+  console.log(diff)
+}
+
+// part 2
+{
+  const rawInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+  const {left, right} = cleanInput(rawInput).map(row => row.split(/\s+/).map(Number)).reduce((agg, curr) => {
+    const [l, r] = curr
+    agg.left.push(l)
+    agg.right.push(r)
+    return agg
+  }, {left: [], right: []})
+  const [sLeft, sRight] = [left.sort(sortNumber), right.sort(sortNumber)]
+  const countRight = sRight.reduce((agg, curr) => {
+    agg[curr] = agg[curr] ? ++agg[curr] : 1
+    return agg
+  }, {})
+  const countLeft = sLeft.reduce((count, curr) => {
+    return count + (curr * (countRight[curr] ?? 0))
+  }, 0)
+
+  console.log(countLeft)
+}
---
+++ 24/utils.ts
@@ -0,0 +1,5 @@
+export const cleanInput = (input: string) => input.split('\n');
+
+export const sum = (a: number, b: number) => a + b
+
+export const sortNumber = (a: number, b: number) => a < b ? -1 : 1;
